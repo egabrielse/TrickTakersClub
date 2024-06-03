861c924306c2c05dfd37509e100ceed60ad236c9
Ethan Gabrielse <39629114+egabrielse@users.noreply.github.com>
2024-06-03 17:49:47 -0500

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import logo from '/logo-shades.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <img src={logo} className="logo" alt="ttc-logo" />
+      <h2>Trick Takers Club</h2>
+    </>
+  )
+}
