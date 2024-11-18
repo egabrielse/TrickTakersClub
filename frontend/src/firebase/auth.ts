35a4af98f10102aec2bd724397b8fe57fb08836a
Ethan Gabrielse <39629114+egabrielse@users.noreply.github.com>
2024-11-18 12:17:46 -0500

    chore: remove debug log

--- frontend/src/firebase/auth.ts
+++ frontend/src/firebase/auth.ts
@@ -5,7 +5,6 @@ const auth = getAuth(firebase);
 
 // If the VITE_FIREBASE_AUTH_EMULATOR_HOST environment variable is set, connect to the local Firebase Auth emulator
 if (import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST) {
-  console.debug(import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST);
   connectAuthEmulator(auth, import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST);
 }
 
