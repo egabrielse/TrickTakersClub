fd06105b85a55ca50c42266211edd5fe4d5f0394
Ethan Gabrielse <39629114+egabrielse@users.noreply.github.com>
2025-02-05 12:33:53 -0500

    chore: move a couple folders

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -11,12 +11,12 @@ import AccountPage from "./pages/account/AccountPage";
 import AuthContextProvider from "./pages/auth/AuthContextProvider";
 import PrivateRoutes from "./pages/auth/PrivateRoutes";
 import HomePage from "./pages/home/HomePage";
+import RootLayout from "./pages/root/RootLayout";
 import ConnectionProvider from "./pages/table/ConnectionProvider";
 import TablePage from "./pages/table/TablePage";
 import TableStateProvider from "./pages/table/TableStateProvider";
 import MuiThemeProvider from "./providers/MuiThemeProvider";
 import UserStoreProvider from "./providers/UserStoreProvider";
-import RootLayout from "./root/RootLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
