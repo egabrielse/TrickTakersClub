1af03faf0d9c02476e0de188e350baeb409b52ea
Ethan Gabrielse <39629114+egabrielse@users.noreply.github.com>
2025-03-04 13:19:28 -0500

    giveup and cleanup

--- backend/domain/service/worker.go
+++ backend/domain/service/worker.go
@@ -14,8 +14,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const TestBackendChange = "foobar"
-
 // TableWorker represents the service for controlling the table
 type TableWorker struct {
 	Table         *entity.TableEntity
