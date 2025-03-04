7447cc872b0da2cb8730e6554940dfa2f0b3520f
Ethan Gabrielse <39629114+egabrielse@users.noreply.github.com>
2025-03-04 16:05:02 -0500

    fix sass deprecation warning

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   envDir: ".",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern",
+      }
+    }
+  }
 });
