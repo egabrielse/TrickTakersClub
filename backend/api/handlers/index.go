9b2982f86bc7d2fcc7812aeb2d46700640340339
Ethan Gabrielse <39629114+egabrielse@users.noreply.github.com>
2025-03-11 15:35:10 -0400

    update health check

--- backend/api/handlers/index.go
+++ backend/api/handlers/index.go
@@ -9,16 +9,28 @@ import (
 
 type HealthCheckResponseBody struct {
 	FirebaseAuth bool `json:"firebaseAuth"`
+	Ably         bool `json:"ably"`
+	Firestore    bool `json:"firestore"`
 }
 
 // HealthCheck is a handler function that returns the health status of the application
 func HealthCheck(r *http.Request, p httprouter.Params) (code int, body any) {
 	statuses := HealthCheckResponseBody{
 		FirebaseAuth: false,
+		Ably:         false,
+		Firestore:    false,
 	}
 
 	if auth := infrastructure.GetFirebaseAuth(); auth != nil {
 		statuses.FirebaseAuth = true
+	}
+
+	if store := infrastructure.GetFirebaseStore(); store != nil {
+		statuses.Ably = true
+	}
+
+	if ably := infrastructure.GetAblyClient(); ably != nil {
+		statuses.Firestore = true
 	}
 
 	return http.StatusOK, statuses
