bd4b72e617edd2db22ebc6758517ddd288bad49e
Ethan Gabrielse <39629114+egabrielse@users.noreply.github.com>
2025-03-13 15:03:51 -0400

    remove failure

--- frontend/src/utils/user.test.ts
+++ frontend/src/utils/user.test.ts
@@ -6,5 +6,4 @@ test('getInitials', () => {
     expect(getInitials('John Doe')).toBe('JD');
     expect(getInitials('Mr John Doe')).toBe('MJD');
     expect(getInitials('john doe')).toBe('JD');
-    expect(getInitials('john doe')).toBe('J');
 });
