66838d7d8eb95eb4b524d4825758c62f6ee9219b
Ethan Gabrielse <39629114+egabrielse@users.noreply.github.com>
2025-06-04 15:42:08 -0400

    check if session exists first

--- backend/play/routing/handlers/connect.go
+++ backend/play/routing/handlers/connect.go
@@ -5,41 +5,31 @@ import (
 	"net/http"
 	"play/app/client"
 	"play/socket"
+	"storage/repository"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func Connect(w http.ResponseWriter, r *http.Request, p httprouter.Params) (int, any) {
+	// Extract the user ID and session ID from the request parameters.
 	uid := p.ByName("uid")
 	sessionID := p.ByName("sessionId")
-
-	// Upgrade the connection to a websocket connection.
-	conn, err := socket.NewConnectionUpgrader().Upgrade(w, r, nil)
-	if err != nil {
+	// Check if the session exists in redis.
+	sessionRepo := repository.GetSessionRepo()
+	if exists, err := sessionRepo.Exists(r.Context(), sessionID); err != nil {
 		return http.StatusInternalServerError, err
+	} else if !exists {
+		return http.StatusNotFound, nil
+	} else {
+		// Upgrade the connection to a websocket connection.
+		conn, err := socket.NewConnectionUpgrader().Upgrade(w, r, nil)
+		if err != nil {
+			return http.StatusInternalServerError, err
+		}
+		// Create a new socket and connect it.
+		socket := client.NewClient(uid, sessionID, conn)
+		socket.Connect()
+		// Return ResponseWriteHandled to indicate response writing is handled
+		return request.ResponseWriteHandled, nil
 	}
-	// Create a new socket and connect it.
-	socket := client.NewClient(uid, sessionID, conn)
-	socket.Connect()
-	return request.ResponseWriteHandled, nil
-
-	// TODO: uncomment after implementing basic frontend connectivity
-	// // Check if the session exists in redis.
-	// sessionRepo := repository.GetSessionRepo()
-	// if exists, err := sessionRepo.Exists(r.Context(), sessionID); err != nil {
-	// 	return http.StatusInternalServerError, err
-	// } else if !exists {
-	// 	return http.StatusNotFound, nil
-	// } else {
-	// 	// Upgrade the connection to a websocket connection.
-	// 	conn, err := socket.NewConnectionUpgrader().Upgrade(w, r, nil)
-	// 	if err != nil {
-	// 		return http.StatusInternalServerError, err
-	// 	}
-	// 	// Create a new socket and connect it.
-	// 	socket := client.NewClient(uid, sessionID, conn)
-	// 	socket.Connect()
-	// 	// Return ResponseWriteHandled to indicate response writing is handled
-	// 	return request.ResponseWriteHandled, nil
-	// }
 }
