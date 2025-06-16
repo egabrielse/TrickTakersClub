ec7a8c5d7035b3b911085740d9cbd2b21a963c14
Ethan Gabrielse <39629114+egabrielse@users.noreply.github.com>
2025-06-16 14:24:14 -0400

    cleanup

--- backend/common/clients/redis.go
+++ backend/common/clients/redis.go
@@ -2,17 +2,12 @@ package clients
 
 import (
 	"github.com/redis/go-redis/v9"
-	"github.com/sirupsen/logrus"
 )
 
 var redisClient *redis.Client
 
 // InitRedisClient initializes the Redis Client with the given host, port, user, and password
 func InitRedisClient(host string, port string, pass string) *redis.Client {
-	logrus.Info("Initializing Redis client")
-	logrus.Infof("Redis host: %s, port: %s", host, port)
-	logrus.Infof("Redis password: %s", pass)
-
 	client := redis.NewClient(&redis.Options{
 		Addr:     host + ":" + port,
 		Password: pass,
